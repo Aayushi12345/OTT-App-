25aa33523aefa0dac842038952975d4667527625
aayushi <aayushi.agarwal@tothenew.com>
2024-01-03 14:06:33 +0530

    create dummy ui

--- lib/presentation/screens/detail_screen.dart
+++ lib/presentation/screens/detail_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class DetailScreen extends StatelessWidget {
   final String name;
   final int id;
+
   const DetailScreen({
     Key? key,
     required this.name,
@@ -11,18 +13,84 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Detail Screen"),
-      ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(child: Text(name,style: TextStyle(color: Colors.green,fontSize: 25.00,fontWeight: FontWeight.bold),))
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Detail Screen"),
         ),
-      ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child:
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+              Container(
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("First Name",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 25.00,
+                            fontWeight: FontWeight.bold)),
+                    Text("Last Name",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 25.00,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+              Container(
+                child:  Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("First Name",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 25.00,
+                            fontWeight: FontWeight.bold)),
+                    Text("Last Name",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 25.00,
+                            fontWeight: FontWeight.bold)),
+
+                    // TextField(
+                    //   maxLines: 2,
+                    //   decoration: InputDecoration(
+                    //       border: InputBorder.none,
+                    //       hintText: 'Enter Your Last Name'),
+                    // )
+                  ],
+                ),
+              )
+            ]),
+          ),
+        )
+      )
     );
   }
 }
+
+// body: Column(
+// children: [
+// CustomTextView(context),
+// // SearchBar(),
+// Expanded(
+// child: ListView.builder(
+// itemCount: 10,
+// itemBuilder: (context, index) {
+// return new GestureDetector(
+// onTap: () {
+// print("tapped");
+// },
+// child: new Padding(
+// padding: const EdgeInsets.all(8.0),
+// child: new Container(
+// color: Colors.grey,
+// height: 100.0,
+// ),
+// ),
+// );
