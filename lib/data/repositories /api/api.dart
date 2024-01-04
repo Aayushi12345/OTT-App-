00e1578e7598cc2e8a9cb486a1f3eb0993bb91d4
aayushi <aayushi.agarwal@tothenew.com>
2024-01-04 19:01:29 +0530

    add carousel lib - done

--- lib/data/repositories /api/api.dart
+++ lib/data/repositories /api/api.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:ott_app/utils/constant.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class API {
   final Dio _dio = Dio();
 
   API() {
-    _dio.options.baseUrl = "https://api.themoviedb.org/3/trending/movie/week?";
+    _dio.options.baseUrl = Constant.BASE_URL;
     _dio.interceptors.add(PrettyDioLogger());
   }
 
---
+++ lib/utils/constant.dart
@@ -0,0 +1,5 @@
+class Constant {
+  static const BASE_URL = "https://api.themoviedb.org/3/trending/movie/week?";
+  static const GET_MOVIES =
+      "api_key=060e7c76aff06a20ca4a875981216f3f&page=1";
+}
