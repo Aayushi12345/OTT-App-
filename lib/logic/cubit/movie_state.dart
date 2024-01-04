ca9772fe4ac22a8a2684d094bfa503173aeaea42
aayushi <aayushi.agarwal@tothenew.com>
2024-01-04 13:18:29 +0530

    Add get it library

--- lib/logic/cubit/movie_state.dart
+++ lib/logic/cubit/movie_state.dart
@@ -1,13 +1,17 @@
 import 'package:ott_app/data/models/movie_model.dart';
+import 'package:equatable/equatable.dart';
 
 abstract class MovieState {}
 
 class LoadingState extends MovieState {}
 
-class LoadedState extends MovieState {
+class LoadedState extends MovieState with EquatableMixin {
   final MovieModel posts;
 
   LoadedState(this.posts);
+
+  @override
+  List<Object?> get props =>[posts];
 }
 
 class ErrorState extends MovieState {
