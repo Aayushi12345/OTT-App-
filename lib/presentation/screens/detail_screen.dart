157411e2d49b79d84481c05bb7855845f1578f67
aayushi <aayushi.agarwal@tothenew.com>
2024-01-15 15:31:49 +0530

    detail screen - done

--- lib/presentation/screens/detail_screen.dart
+++ lib/presentation/screens/detail_screen.dart
@@ -1,7 +1,5 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:ott_app/data/models/movie_model.dart';
 import 'package:ott_app/utils/constant.dart';
 
@@ -20,62 +18,58 @@ class DetailScreen extends StatelessWidget {
           title: Text(movie.title.toString()),
         ),
         body: SafeArea(
-
           child: Padding(
-
             padding: const EdgeInsets.all(15.0),
             child:
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                          child: SizedBox(
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Container(
+                  child: SizedBox(
+                height: 200,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Image.network(
+                        Constant.IMAGE_BASE_URL + movie.posterPath.toString(),
+                        fit: BoxFit.cover),
+                    ClipRRect(
+                      // Clip it cleanly.
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Container(
+                          color: Colors.grey.withOpacity(0.1),
+                          alignment: Alignment.center,
+                          child: Image.network(
+                            Constant.IMAGE_BASE_URL +
+                                movie.posterPath.toString(),
+                            fit: BoxFit.fill,
                             height: 200,
-                            child: Stack(
-                              fit: StackFit.expand,
-                              children: [
-                                Image.network( Constant.IMAGE_BASE_URL +
-                                    movie.posterPath
-                                        .toString(), fit: BoxFit.cover),
-                                ClipRRect( // Clip it cleanly.
-                                  child: BackdropFilter(
-                                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                                    child: Container(
-                                      color: Colors.grey.withOpacity(0.1),
-                                      alignment: Alignment.center,
-                                      child: Image.network(   Constant.IMAGE_BASE_URL +
-                                    movie.posterPath
-                                        .toString(),
-                                    fit: BoxFit.fill,
-                                    height: 200,
-                                  ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )
+                          ),
+                        ),
                       ),
-
-                      SizedBox(height: 25,),
-
-                      const Text("OverView",
+                    ),
+                  ],
+                ),
+              )),
+              SizedBox(
+                height: 25,
+              ),
+              const Text("OverView",
                   style: TextStyle(
                       color: Colors.black,
                       fontSize: 15.00,
                       fontWeight: FontWeight.bold)),
               Text(movie.overview.toString()),
-              const SizedBox(height: 25,),
-
-               Row(
+              const SizedBox(
+                height: 25,
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Row(
                     children: [
                       const Text("Release Date : ",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18.00)),
+                          style:
+                              TextStyle(color: Colors.black, fontSize: 18.00)),
                       Center(
                         child: Text(movie.releaseDate.toString(),
                             style: const TextStyle(
@@ -87,10 +81,10 @@ class DetailScreen extends StatelessWidget {
                   ),
                   Row(
                     children: [
-                      const Text("Rating : " ,
+                      const Text("Rating : ",
                           style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18.00,
+                            color: Colors.black,
+                            fontSize: 18.00,
                           )),
                       Center(
                         child: Text(movie.voteAverage.toString(),
@@ -103,11 +97,8 @@ class DetailScreen extends StatelessWidget {
                   ),
                 ],
               ),
-
             ]),
           ),
-        )
-
-    );
+        ));
   }
 }
